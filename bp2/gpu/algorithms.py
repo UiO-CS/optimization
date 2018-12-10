e45fa481e6825a3a738198dc3d3d98a3088b50e4
Kristian Monsen Haug <kristian.haug95@gmail.com>
2018-12-10 10:48:51 +0100

    Base skeleton, and implement operator used for MRI

---
+++ bp2/gpu/algorithms.py
@@ -0,0 +1,93 @@
+from abc import ABC, abstractmethod
+from .proximal import FISTAProximal
+
+import numpy as np
+from math import sqrt
+
+class LASSOGradient:
+
+    def __init__(self, op, measurements):
+        self.op = op
+        self.measurements = measurements
+
+    def __call__(self, y):
+        # TODO: Implement
+        pass
+        # return 2*self.op(self.op(y) - self.measurements, adjoint=True)
+
+
+class Algorithm(ABC):
+
+    @abstractmethod
+    def run(self):
+        pass
+
+    def __call__(self, *args):
+        return self.run(*args)
+
+
+class FISTA(Algorithm):
+
+    def __init__(self, gradient, L, lam):
+        super().__init__()
+        # self.L = L
+        # self.lam = lam
+        # self.proximal = FISTAProximal(gradient, L, lam)
+        # self.result = None
+
+    def run(self, n_iter=1000, initial_x=None):
+        # TODO: Implement
+        pass
+        # if initial_x is None and self.result is None:
+        #     raise ValueError('Need an initial value')
+        # elif self.result != None:
+        #     initial_x = self.result
+
+        # x_old = initial_x
+        # y = x_old
+        # t_old = 1
+
+        # for i in range(1,n_iter+1):
+        #     print(i)
+        #     x = self.proximal(y)
+        #     t = (1 + sqrt(1 + 5*t_old**2))/2.0
+        #     y = x + (t_old - 1)/t * (x - x_old)
+
+        #     x_old = x
+        #     t_old = t
+
+        # self.result = x_old
+        # return x_old
+
+class PrimalDual(Algorithm):
+
+    def __init__(self, op, prox_f_star, prox_g, theta, tau, sigma, eta):
+        self.op = op
+        self.prox_f_star = prox_f_star
+        self.prox_g = prox_g
+        self.theta = theta
+        self.tau = tau
+        self.sigma = sigma
+        self.eta = eta
+
+    def run(self, n_iter, initial_x=None, initial_ksi=None):
+        # TODO: Implement
+        pass
+
+        # x_old = initial_x
+        # x_line = x_old
+        # ksi = np.zeros_like(x_old)
+
+        # for i in range(n_iter):
+        #     print(i)
+        #     ksi = self.prox_f_star(ksi + self.sigma*self.op(x_line))
+        #     x = self.prox_g(x_old - self.tau*self.op(ksi, adjoint=True))
+        #     x_line = x + self.theta*(x - x_old)
+
+        #     x_old = x
+
+
+        # self.result = x
+        # return x
+
+
---
+++ bp2/gpu/operators.py
@@ -0,0 +1,76 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+import tensorflow as tf
+
+from tfwavelets.nodes import dwt2d, idwt2d
+
+class LinearOperator(ABC):
+
+    @abstractmethod
+    def forward(self, x):
+        """Computes Ax"""
+        pass
+
+    @abstractmethod
+    def adjoint(self, x):
+        """Computes A*x"""
+        pass
+
+    def __call__(self, x, adjoint=False):
+        """Convenience method"""
+        if adjoint:
+            return self.adjoint(x)
+        return self.forward(x)
+
+    def sample(self, x):
+        """In case one wants to sample differently than with the forward transform e.g.
+        if A = PFW*, and we want to sample with PF.
+        """
+        return self.forward(x)
+
+
+
+class MRIOperator(LinearOperator):
+    """A = PFW*"""
+
+    def __init__(self, samp_patt, wavelet, levels, shift_samp_patt=True):
+        super().__init__()
+
+        self.wavelet = wavelet
+        self.levels = levels
+        self.samp_patt = samp_patt
+
+
+    def forward(self, x):
+        real_idwt = idwt2d(tf.real(x), self.wavelet, self.levels)
+        imag_idwt = idwt2d(tf.imag(x), self.wavelet, self.levels)
+        result = tf.complex(real_idwt, imag_idwt)
+
+        # TODO: is this right?
+        result = tf.transpose(result, [2,0,1])
+
+        # TODO Is this right?
+        result = tf.complex(1.0/tf.sqrt(tf.size(result, out_type=tf.float32)), 0.0) * tf.fft2d(result)
+        result = tf.transpose(result, [1,2,0])
+
+        result = tf.where(self.samp_patt, result, tf.zeros_like(result))
+        return result
+
+    def adjoint(self, x):
+        result = tf.where(self.samp_patt, x, tf.zeros_like(x))
+        result = tf.transpose(result, [2,0,1]) # [channels, height, width]
+        result = tf.complex(tf.sqrt(tf.size(result, out_type=tf.float32)), 0.0) * tf.ifft2d(result)
+        result = tf.transpose(result, [1,2,0]) # [height, width, channels]
+        real_dwt = dwt2d(tf.real(result), self.wavelet, self.levels)
+        imag_dwt = dwt2d(tf.imag(result), self.wavelet, self.levels)
+        result = tf.complex(real_dwt, imag_dwt)
+        return result
+
+    def sample(self, x):
+        result = tf.transpose(x, [2,0,1])
+        # TODO Is this right?
+        result = tf.complex(1.0/tf.sqrt(tf.size(result, out_type=tf.float32)), 0.0) * tf.fft2d(result)
+        result = tf.transpose(result, [1,2,0])
+        result = tf.where(self.samp_patt, result, tf.zeros_like(result))
+        return result
