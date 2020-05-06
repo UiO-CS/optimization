bb5327bcdbf5843625d6c656fb28fa0365aa9ce5
Kristian Monsen Haug <kristian.haug95@gmail.com>
2020-05-06 17:50:49 +0200

    Change weight generating function to handle input from sparsity estimation

--- optimization/utils/weights.py
+++ optimization/utils/weights.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 def generate_weight_matrix(N, weights, dtype):
-    '''Creates a weight matrix where each weight is put into a wavelet level
+    '''Creates a weight matrix where each weight is put into a wavelet level, 
+       with the first corresponding to the low res coeffs
     
     Paramters:
         N: integer, power of 2 width and height of result
@@ -11,7 +12,7 @@ def generate_weight_matrix(N, weights, dtype):
 
     result = np.zeros((N,N), dtype=dtype)
 
-    for w in weights:
+    for w in reversed(weights):
         result[:N,:N] = w
         N = N >> 1
 
