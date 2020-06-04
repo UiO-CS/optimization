Fix computation of weights

optimization/utils/weights.py
@@ -14,7 +14,7 @@ def generate_weight_matrix(N, weights, dtype):
 
     s = np.sum(weights)
     for sj in reversed(weights):
-        result[:N,:N] = s/np.sqrt(sj)
+        result[:N,:N] = np.sqrt(s/sj)
         N = N >> 1
 
 