f49f486b416fa87f8cfe9687500f45bc23439f56
Vegard Antun <vegarant@student.matnat.uio.no>
2020-11-06 13:45:44 +0100

    Created a new branch with the exponential decay algorithm

--- optimization/gpu/algorithms.py
+++ optimization/gpu/algorithms.py
@@ -15,29 +15,6 @@ def dtype_to_cdtype(dtype):
         cdtype = tf.complex128
     return cdtype
 
-class LASSOGradient:
-    """
-    The gradient of || Ay - b ||^2. i.e. 2A*(Ay - b)
-
-    For the problem ||Ay - b||^2 + \lambda || y ||_1
-    """
-
-    def __init__(self, op, measurements):
-        """
-        Initializer
-
-        Arguments
-            op: A instance of a LinearOperator, e.g. the A matrix
-            measurements: Tensor (3D?)
-        """
-        self.op = op
-        self.measurements = measurements
-
-    def __call__(self, y):
-        """Evaluation of the gradient"""
-        # return self.op(self.op(y) - self.measurements, adjoint=True)
-        return tf.complex(2.0, 0.0)*self.op(self.op(y) - self.measurements, adjoint=True)
-
 
 class Algorithm(ABC):
     """Abstract class for an algorithm."""
@@ -51,228 +28,14 @@ class Algorithm(ABC):
         return self.run(*args)
 
 
-class FISTA(Algorithm):
-    """The FISTA algorithm
 
-    For ease of use, the proximal operator is set to FISTAProximal and is thus fixed to
-    F(z) = f(z) + g(z)
-    where
-    f(z) = ||Az - b||_2^2 and
-    g(x) = lambda * ||z||_1
-    """
-
-    def __init__(self, gradient):
-        """
-        Arguments:
-            gradient: callable. Gradient of the function
-            L: Lipschitz constant
-            lam: Lambda parameter in LASSO.
-        """
-        super().__init__()
-        self.L = tf.compat.v1.placeholder(tf.float32, shape=(), name='L')
-        self.lam = tf.compat.v1.placeholder(tf.float32, shape=(), name='lambda')
-        self.proximal = FISTAProximal(gradient)
-        self.proximal.set_parameters(self.L, self.lam)
-
-        self.result = None
-
-    def body(self, x_old, y_old, t_old):
-        """
-        The iteration step
-
-        Arguments
-            x_old: Tensor. x from the previous step
-            y_old: Tensor. y from the previous step
-            t_old: Tensor. t from the previous step
-
-        Returns:
-            Tensors x, y and t after one iteration of Fista
-        """
-
-        x = self.proximal(y_old)
-        t = (1 + tf.sqrt(1  + 4 * t_old**2))/2.0
-        y = x + tf.complex((t_old - 1.0)/t, 0.0) * (x - x_old)
-
-        return x, y, t
-
-
-    def run(self, initial_x=None):
-        """
-        Runs FISTA
-
-        Arguments:
-            n_iter: int. Number of iterations. Default 1000
-            initial_x: Tensor. Initial x
-
-        Returns:
-            x after n_iter iterations of FISTA
-        """
-        if initial_x is None and self.result is None:
-            raise ValueError('Need an initial value')
-        elif self.result != None:
-            initial_x = self.result
-
-        x_old = initial_x
-        y = x_old
-        t_old = 1
-
-        # Iterate until n_iter iteration is reached
-        x, y, t = tf.while_loop(lambda *args: True,
-                                self.body,
-                                (initial_x, initial_x, 1.0),
-                                maximum_iterations=tf.compat.v1.placeholder(tf.float32,
-                                                                  shape=(),
-                                                                  name='n_iter'))
-
-        # TODO: Set result
-        return x
-
-class PrimalDual(Algorithm):
-    """Implementation of the Chambolle--Pock algorithm
-
-    Optimizing F(Ax) + G(x)"""
-
-    def __init__(self, op, prox_f_star, prox_g, theta, tau, sigma, eta, dtype):
-        """
-        Arguments
-            op: LinearOperator (The matrix A)
-            prox_f_star: ProximalOperator. prox_{F*} (* denoting convex conjugate).
-                         Example found in proximal module
-            prox_g: ProximalOperator. prox_{G}
-            theta, tau, sigma, eta: parameters
-        """
-        self.op = op
-        self.prox_f_star = prox_f_star
-        self.prox_g = prox_g
-
-        #self.theta = tf.compat.v1.placeholder(tf.float32, shape=(), name='theta')
-        #self.tau = tf.compat.v1.placeholder(tf.float32, shape=(), name='tau')
-        #self.sigma = tf.compat.v1.placeholder(tf.float32, shape=(), name='sigma')
-        #self.eta = tf.compat.v1.placeholder(tf.float32, shape=(), name='eta')
-        
-        self.theta = theta;
-        self.tau = tau;
-        self.sigma = sigma;
-        self.eta = eta;
-        self.dtype = dtype;
-        self.cdtype = dtype_to_cdtype(dtype);
-        #self.prox_f_star.set_parameters(self.theta, self.tau, self.sigma, self.eta)
-        #self.prox_g.set_parameters(self.theta, self.tau, self.sigma, self.eta)
-
-    def body(self, x_old, x_line, ksi):
-        """Similar as FISTA.body"""
-        ksi = self.prox_f_star(ksi + tf.cast(self.sigma, self.cdtype)*self.op(x_line))
-        x = self.prox_g(x_old - tf.cast(self.tau, self.cdtype)*self.op(ksi, adjoint=True))
-        x_line = x + tf.cast(self.theta, self.cdtype)*(x - x_old)
-
-        return x, x_line, ksi
-
-    def run(self, n_iter, initial_x=None, initial_ksi=None):
-        """Similar as FISTA.body"""
-
-        # Initial values
-        x = initial_x
-        x_line = x
-        ksi = tf.zeros_like(x)
-
-        x, x_line, ksi = tf.while_loop(lambda *args: True,
-                                       self.body,
-                                       (x, x_line, ksi),
-                                       maximum_iterations=n_iter)
-
-        return x
-
-
-class SquareRootLASSO(Algorithm):
-
-    def __init__(self, op, prox1, prox2, measurements, tau, sigma, lam, dtype=tf.float32):
-        self.cdtype = dtype_to_cdtype(dtype)
-        self.dtype = dtype
-        self.op = op
-        self.prox1 = prox1
-        self.prox2 = prox2
-
-        self.measurements = measurements
-
-        # Casting is necessary for types to match in the body
-        self.tau = tf.cast(tau, self.cdtype)
-        self.sigma = tf.cast(sigma, self.cdtype)
-
-    def body(self, x_old, y_old):
-
-        x_new = self.prox1(x_old - self.tau * self.op(y_old, adjoint=True))
-        y_new = self.prox2(y_old + self.sigma * self.op(2*x_new - x_old) - self.sigma*self.measurements)
-
-        return x_new, y_new
-
-    def run(self, initial_x=None, initial_y=None):
-        """Similar as FISTA.body"""
-        
-
-        # Initial values
-        x = initial_x
-        y = tf.zeros_like(x)
-
-        if initial_y:
-            y = initial_y
-
-        x_result, y_result = tf.while_loop(lambda *args: True,
-                                           self.body,
-                                           (x, y),
-                                           maximum_iterations=tf.compat.v1.placeholder(tf.int32, shape=(), name='n_iter'),
-                                           )
-
-
-        return x_result
-
-class SR_LASSO_old(Algorithm):
-
-    def __init__(self, measurements, op, n_iter, tau, sigma, lam, weights_mat, dtype=tf.float32):
-        self.cdtype = dtype_to_cdtype(dtype)
-        self.dtype = dtype
-        self.op = op
-        self.prox1 = WeightedL1Prox(weights_mat, lam*tau, dtype=self.dtype)
-        self.prox2 = SQLassoProx2(dtype=self.dtype)
-        self.n_iter = n_iter;
-        self.measurements = measurements
-
-        # Casting is necessary for types to match in the body
-        self.tau = tf.cast(tau, self.cdtype)
-        self.sigma = tf.cast(sigma, self.cdtype)
-
-    def body(self, x_old, y_old):
-
-        x_new = self.prox1(x_old - self.tau * self.op(y_old, adjoint=True))
-        y_new = self.prox2(y_old + self.sigma * self.op(2*x_new - x_old) - self.sigma*self.measurements)
-
-        return x_new, y_new
-
-    def run(self, initial_x, initial_y=None):
-        """Similar as FISTA.body"""
-        
-
-        # Initial values
-        x = initial_x
-        y = tf.zeros_like(x)
-
-        if initial_y:
-            y = initial_y
-
-        x_result, y_result = tf.while_loop(lambda *args: True,
-                                           self.body,
-                                           (x, y),
-                                           maximum_iterations=self.n_iter,
-                                           )
-
-
-        return x_result
 
 class SR_LASSO_Ergodic(Algorithm):
 
     def __init__(self, op, p_iter, tau, sigma, lam, weights_mat, dtype=tf.float32):
         self.cdtype = dtype_to_cdtype(dtype)
         self.dtype = dtype
-        
+
         self.op = op
         self.prox1 = WeightedL1Prox(weights_mat, lam*tau, dtype=self.dtype)
         self.prox2 = SQLassoProx2(dtype=self.dtype)
@@ -293,11 +56,10 @@ class SR_LASSO_Ergodic(Algorithm):
 
     def run(self, measurements,  initial_x, initial_y=None):
         """Similar as FISTA.body"""
-        
 
         # Initial values
         x = initial_x
-        y = tf.zeros_like(x)
+        y = tf.zeros_like(initial_x)
         x_sum = tf.zeros_like(x)
 
         if initial_y:
@@ -306,7 +68,7 @@ class SR_LASSO_Ergodic(Algorithm):
         measurements, x_result, y_result, x_ergodic = tf.while_loop(lambda *args: True,
                                            self.body,
                                            (measurements, x, y, x_sum),
-                                           maximum_iterations=self.p_iter #tf.placeholder(tf.int32, shape=(), name='n_iter'),
+                                           maximum_iterations=self.p_iter 
                                            )
 
         x_ergodic = x_ergodic/self.p_iter_complex;
@@ -315,11 +77,12 @@ class SR_LASSO_Ergodic(Algorithm):
 
 class SR_LASSO_exponential(Algorithm):
 
-    def __init__(self, measurements, op, p_iter, tau, sigma, lam, weights_mat, L_A, eps_0, delta, dtype=tf.float32):
+    def __init__(self, measurements, x0, op, p_iter, tau, sigma, lam, weights_mat, L_A, eps_0, delta, dtype=tf.float32):
         self.cdtype = dtype_to_cdtype(dtype)
         self.dtype = dtype
 
         self.measurements = measurements
+        self.x0 = x0
         self.op = op
         self.p_iter = p_iter
         self.p_iter_float = tf.cast(p_iter, self.dtype)
@@ -335,11 +98,10 @@ class SR_LASSO_exponential(Algorithm):
     def body(self, x, eps_k):
 
         eps_k_new = math.exp(-1)*(self.delta + eps_k) 
-        alpha_k = (eps_k*self.p_iter_float) / (2*self.L_A);
+        alpha_k = (eps_k_new*self.p_iter_float) / (2.0*self.L_A);
         alpha_k = tf.cast(alpha_k, self.cdtype);
 
-        # TODO check division with complex numbers in TF.
-        x_new = alpha_k*self.inner_it(self.measurements/alpha_k, x/alpha_k, )
+        x_new = alpha_k*self.inner_it(self.measurements/alpha_k, x/alpha_k)
 
         return x_new, eps_k_new
 
@@ -347,13 +109,13 @@ class SR_LASSO_exponential(Algorithm):
     def run(self, n_iter):
 
         # Initial values
-        get_right_dimensions = self.op.sample(self.measurements, adjoint=True)
+        # get_right_dimensions = self.op.sample(self.measurements, adjoint=True)
 
-        x0 = tf.zeros_like(get_right_dimensions);
+        # x0 = tf.zeros_like(get_right_dimensions);
 
         x_result, eps_n = tf.while_loop(lambda *args: True,
                                            self.body,
-                                           (x0, self.eps_0),
+                                           (self.x0, self.eps_0),
                                            maximum_iterations=n_iter)
 
         return x_result
--- optimization/gpu/proximal.py
+++ optimization/gpu/proximal.py
@@ -8,146 +8,8 @@ class ProximalOperator(ABC):
     def __call__(self, x):
         pass
 
-
-
-class PDProxOperator(ProximalOperator):
-    """Primal-Dual prox operator. Abstract Class for the Primal-Dual algorithm"""
-
-    @abstractmethod
-    def set_parameters(self, theta, tau, sigma, eta):
-        """Paramters are placeholders to the parameters"""
-        pass
-
-class FISTAProxOperator(ProximalOperator):
-
-    @abstractmethod
-    def set_parameters(self, L, lam):
-        """Paramters are placeholders to the parameters"""
-        pass
-
-        
-
-# _        _    ____ ____   ___
-#| |      / \  / ___/ ___| / _ \
-#| |     / _ \ \___ \___ \| | | |
-#| |___ / ___ \ ___) |__) | |_| |
-#|_____/_/   \_\____/____/ \___/
-class FISTAProximal(ProximalOperator):
-    '''Proximal operator for Fista on lasso'''
-
-    def __init__(self, gradient):
-        super().__init__()
-        self.gradient = gradient
-        self.L = None
-        self.lam = None
-
-    def base_call(self, z):
-        return tf.sign(z)*tf.complex(tf.nn.relu(tf.abs(z) - self.lam/self.L), 0.0)
-
-    def __call__(self, y):
-        pass
-        b = y - self.gradient(y)/tf.cast(self.L, tf.complex64)
-        return self.base_call(b)
-
-    def set_parameters(self, L, lam):
-        self.L = L
-        self.lam = lam
-
-
-
-# ____  ____  ____  _   _
-#| __ )|  _ \|  _ \| \ | |
-#|  _ \| |_) | | | |  \| |
-#| |_) |  __/| |_| | |\  |
-#|____/|_|   |____/|_| \_|
-class BPDNFStar(PDProxOperator):
-    """prox_{F*}
-
-    Where F* is the convex conjugate for when F(ksi) is the function that is 1 when
-    ||ksi - y|| <= eta and 0 otherwise
-
-    Equation can be found on p 485 of A mathematical introduction to
-    compressive sensing"""
-
-    def __init__(self, measurements, sigma, eta, dtype=tf.float32):
-        # TODO: Type might be wrong
-        self.sigma = sigma
-        self.eta = eta
-        self.y = measurements
-        self.dtype = dtype;
-        if dtype == tf.float32:
-            self.cdtype = tf.complex64
-        elif dtype == tf.float64:
-            self.cdtype = tf.complex128
-
-    def __call__(self, ksi):
-        norm_expression = ksi - tf.cast(self.sigma, self.cdtype)*self.y
-        norm_val = tf.cast(tf.norm(norm_expression), self.dtype)
-        compare_val = self.eta*self.sigma
-
-        result = tf.cond(
-            tf.cast(norm_val, self.dtype) < compare_val,
-            lambda: tf.zeros_like(ksi),
-            lambda: tf.cast(tf.constant(1.0, dtype=self.dtype) - compare_val/norm_val, self.cdtype) * norm_expression
-        )
-
-        return result
-
-    def set_parameters(self, theta, tau, sigma, eta):
-        self.sigma = sigma
-        self.eta = eta
-
-
-class BPDNG(PDProxOperator):
-    """prox_G when G(z) = ||z||_1
-
-    Equation (15.23) in A mathematical introduction to compressive sensing"""
-
-    def __init__(self, tau, dtype=tf.float32):
-        self.tau = tau
-        self.dtype = dtype;
-        
-    def __call__(self, z):
-        return tf.sign(z)*tf.complex(tf.nn.relu(tf.abs(z) - self.tau), tf.constant(0.0, dtype=self.dtype))
-
-    def set_parameters(self, theta, tau, sigma, eta):
-        self.tau = tau
-
-
-
-#  ____   ___  _   _   _    ____  _____ ____   ___   ___ _____ 
-# / ___| / _ \| | | | / \  |  _ \| ____|  _ \ / _ \ / _ \_   _|
-# \___ \| | | | | | |/ _ \ | |_) |  _| | |_) | | | | | | || |  
-#  ___) | |_| | |_| / ___ \|  _ <| |___|  _ <| |_| | |_| || |  
-# |____/ \__\_\\___/_/   \_\_| \_\_____|_| \_\\___/ \___/ |_|  
-#                                                              
-#  _        _    ____ ____   ___  
-# | |      / \  / ___/ ___| / _ \ 
-# | |     / _ \ \___ \___ \| | | |
-# | |___ / ___ \ ___) |__) | |_| |
-# |_____/_/   \_\____/____/ \___/ 
-#                                 
-
-# TODO Very rushed naming and computations
-class SQLassoProx1(ProximalOperator):
-
-    def __init__(self, dtype=tf.float32):
-        self.tau = None
-        self.lam = None
-        self.dtype = dtype
-
-    def __call__(self, z):
-        return tf.sign(z)*tf.complex(tf.nn.relu(tf.abs(z) - self.tau*self.lam), tf.constant(0.0, dtype=self.dtype))
-
-
-    def set_parameters(self, tau, lam):
-        self.tau = tau
-        self.lam = lam
-
-
 class SQLassoProx2(ProximalOperator):
 
-
     def __init__(self, dtype=tf.float32):
         self.dtype = dtype
         if dtype == tf.float32:
@@ -155,9 +17,9 @@ class SQLassoProx2(ProximalOperator):
         elif dtype == tf.float64:
             self.cdtype = tf.complex128
 
-
     def __call__(self, z):
-        return z * tf.cast(tf.minimum(tf.constant(1.0, dtype=self.dtype), tf.abs(1.0/tf.norm(z))), self.cdtype);
+        tf_norm = tf.compat.v1.norm(z);
+        return z * tf.cast(tf.minimum(tf.constant(1.0, dtype=self.dtype), tf.abs(1.0/tf_norm)), self.cdtype);
 
 
 class WeightedL1Prox(ProximalOperator):
@@ -174,10 +36,14 @@ class WeightedL1Prox(ProximalOperator):
         self.weights = weights
         self.s = s
         self.dtype = dtype
+        if dtype == tf.float32:
+            self.cdtype = tf.complex64
+        elif dtype == tf.float64:
+            self.cdtype = tf.complex128
 
 
     def __call__(self, z):
-        return tf.sign(z)*tf.complex(tf.nn.relu(tf.abs(z) - self.weights*self.s), tf.constant(0.0, dtype=self.dtype))
+        return tf.sign(z)*tf.cast(tf.nn.relu(tf.abs(z) - self.weights*self.s), dtype=self.cdtype)
 
 
 
